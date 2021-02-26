4fa9374eb7f31056ba9827a223b5bee401ebe924
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-02-25 18:37:37 -0800

    implement simple adder

--- Program.fs
+++ Program.fs
@@ -3,12 +3,27 @@
 open System
 open NUnit.Framework
 open FsUnit
+open System
 
 [<Test>]
 let ``test framework`` () =
-    1 |> should equal 1
+    5 + 1 |> should equal 6
+
+let toValue = function
+| (true, v) -> v
+| (_, _) -> 0
+
+let toInt : String -> Int32 = (Int32.TryParse >> toValue)
+
+let addArgs (argv: string[]) =
+    argv
+    |> Seq.map toInt
+    |> Seq.reduce (+)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let result = if argv.Length > 0 then argv |> addArgs else 0
+    printfn "What's your name? "
+    let name = Console.ReadLine()
+    printfn "Hi %s, the result is %d" name result
     0 // return an integer exit code
